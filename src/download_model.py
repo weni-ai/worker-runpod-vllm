572891e603e893de693263e26319f789b3b23b14
Icaro Rodrigues <icaro.rodrigues@protonmail.com>
2023-12-04 19:08:19 -0300

    add local dir model

--- src/download_model.py
+++ src/download_model.py
@@ -5,6 +5,7 @@ from huggingface_hub import snapshot_download
 HUGGING_FACE_HUB_TOKEN = os.environ.get('HUGGING_FACE_HUB_TOKEN', None)
 MODEL_NAME = os.environ.get('MODEL_NAME')
 MODEL_REVISION = os.environ.get('MODEL_REVISION', "main")
+MODEL_BASE_PATH = os.environ.get('MODEL_BASE_PATH', '/runpod-volume/')
 
 # Download the model from hugging face
 download_kwargs = {}
@@ -15,5 +16,6 @@ if HUGGING_FACE_HUB_TOKEN:
 snapshot_download(
     repo_id=MODEL_NAME,
     revision=MODEL_REVISION,
+    local_dir=f"{MODEL_BASE_PATH}{MODEL_NAME.split('/')[1]}",
     **download_kwargs
 )
